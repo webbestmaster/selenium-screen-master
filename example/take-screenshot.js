e6ad3227163425ba6cf3800de921a33d03b30c18
Dmitry Turovtsov <web.best.master@gmail.com>
2017-02-08 19:18:38 +0300

    update example

--- example/take-screenshot.js
+++ example/take-screenshot.js
@@ -1,57 +1,36 @@
-const CWD = __dirname;
-const path = require('path');
 const Ssm = require('selenium-screen-master');
 const ssm = new Ssm();
-
-const jar = require('selenium-server-standalone-jar');
-jar.path = path.join(CWD, 'selenium-server-standalone-3.0.1.jar');
-
-const SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
-
-const server = new SeleniumServer(jar.path, {
-    port: 4444
-});
 
 const SERVER_URL = 'http://statlex.github.io/';
 const WEB_DRIVER_SERVER_URL = 'http://localhost:4444/wd/hub';
 
 let WebDriver = require('selenium-webdriver');
 let byCss = WebDriver.By.css;
-let driver;
+let driver = new WebDriver
+    .Builder()
+    .usingServer(WEB_DRIVER_SERVER_URL)
+    .withCapabilities({'browserName': 'chrome'})
+    .build();
 
-server.start()
-    .then(() => {
+driver.get(SERVER_URL);
 
-        driver = new WebDriver
-            .Builder()
-            .usingServer(WEB_DRIVER_SERVER_URL)
-            .withCapabilities({'browserName': 'chrome'})
-            .build();
+ssm.setDriver(driver);
 
-        driver.get(SERVER_URL);
-        ssm.setDriver(driver);
+ssm
+    .takeScreenshotOfSelector('#ancient-empire-strike-back')
+    .then(image => {
+        // image base64
+        console.log(image);
+    });
 
-// USE like this
+// OR
 
-        return ssm
-            .takeScreenshotOfSelector('#ancient-empire-strike-back')
-            .then(image => {
-                // image base64
-                console.log(image);
-            });
+ssm
+    .takeScreenshotOfElement(driver.findElement(byCss('#ancient-empire-strike-back')))
+    .then(image => console.log(image));
 
-// OR like this
+// OR
 
-        return ssm
-            .takeScreenshotOfElement(driver.findElement(byCss('#ancient-empire-strike-back')))
-            .then(image => console.log(image));
-
-// OR like this
-
-        return ssm
-            .takeScreenshotOfArea(80, 200, 500, 300)
-            .then(image => console.log(image));
-
-    })
-    .then(() => driver.quit())
-    .then(() => server.stop());
+ssm
+    .takeScreenshotOfArea(80, 200, 500, 300)
+    .then(image => console.log(image));
